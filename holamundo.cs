031aaed68b5817701aa078e7d91c6512e316e73d
Marcos <marcos@marcos-Inspiron-1545.(none)>
2013-02-18 22:53:38 -0600

    Imprimir en pantalla un parrafo.

---
+++ holamundo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Application
+{
+	public class holamundo
+	{
+		public void main (String[] args)
+		{
+			Console.WriteLine("Hola Mundo");
+			Console.Read();
+		}
+	}
+}
+
